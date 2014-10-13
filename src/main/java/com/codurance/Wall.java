9616f7e3579cad70dccb387a5f230ac42e75cba3
Toby Retallick <toby.retallick@gmail.com>
2014-10-13 13:14:38 +0100

    Started rewriting Wall Class

--- src/main/java/com/codurance/Wall.java
+++ src/main/java/com/codurance/Wall.java
@@ -11,7 +11,17 @@ public class Wall implements Actions {
 
     @Override
     public void execute(String userInput) {
-
+        if(isValid(userInput)) {
+            retrieveUserAccount();
+//            user.getPosts();
+        }
     }
 
+    private User retrieveUserAccount() {
+        return repository.findOrCreate("Alice");
+    }
+
+    private boolean isValid(String userInput) {
+        return userInput.split(" ").length == 1;
+    }
 }
--- src/test/java/com/codurance/unittests/WallShould.java
+++ src/test/java/com/codurance/unittests/WallShould.java
@@ -2,14 +2,21 @@ package com.codurance.unittests;
 
 import com.codurance.Console;
 import com.codurance.Repository;
+import com.codurance.User;
 import com.codurance.Wall;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.mockito.Mockito.mock;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
 
 public class WallShould {
 
+    private static final String POST_1 = "I like beer";
+    private static final String POST_2 = "I like it very much";
     private Wall wall;
     private Repository repository;
     private String VALID_INPUT = "Alice";
@@ -25,7 +32,28 @@ public class WallShould {
     }
 
     @Test public void
-    should_not_execute_if_command_s_not_valid() {
+    not_execute_if_command_is_not_valid() {
+        wall.execute(INVALID_INPUT);
+        verify(repository, never()).findOrCreate(any());
+    }
 
+    @Test public void
+    execute_if_command_is_valid() {
+        wall.execute(VALID_INPUT);
+        verify(repository).findOrCreate(VALID_INPUT);
+    }
+
+    @Test public void
+    instruct_console_to_print_display_the_users_posts() {
+        User alice = mock(User.class);
+        List<String> alicePosts = new ArrayList<>();
+
+        alicePosts.add(POST_1);
+        alicePosts.add(POST_2);
+
+        wall.execute(VALID_INPUT);
+        when(alice.getPosts()).thenReturn(alicePosts);
+
+        verify(console).print(alicePosts);
     }
 }
