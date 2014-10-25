ca9931e7329fc4c54d0bb844b61f065b099ecb4f
Toby Retallick <toby.retallick@gmail.com>
2014-10-25 01:00:06 +0100

    Repository returns a user's posts

--- src/main/java/com/codurance/Repository.java
+++ src/main/java/com/codurance/Repository.java
@@ -1,31 +1,28 @@
 package com.codurance;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.stream.Collectors;
 
-//
-//import java.util.ArrayList;
-//import java.util.HashMap;
-//import java.util.List;
-//import java.util.Map;
-//
+
 public class Repository {
 
     private List<Post> posts = new ArrayList<>();
-    private Map<String, List<String>> followRelationships = new HashMap<>();
+    //private Map<String, List<String>> followRelationships = new HashMap<>();
 
     public void add(Post post) {
         posts.add(post);
     }
 
-    public List<Post> allPosts() {
-        return posts;
-    }
+//    public List<Post> allPosts() {
+//        return posts;
+//    }
 
-    public List<Post> getPostsFor(String userInput) {
-        return null;
+    public List<Post> getPostsFor(String userName) {
+        List<Post> userPosts = posts.stream().
+                                filter(post -> post.getUser() == userName).
+                                collect(Collectors.toList());
+        return userPosts;
     }
 
 //    public void createRelationship(String follower, String followee) {
--- src/test/java/com/codurance/unittests/RepositoryShould.java
+++ src/test/java/com/codurance/unittests/RepositoryShould.java
@@ -1,38 +1,37 @@
-//package com.codurance.unittests;
-//
-//import com.codurance.Clock;
-//import com.codurance.Post;
-//import com.codurance.Repository;
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import static org.hamcrest.Matchers.contains;
-//import static org.hamcrest.collection.IsMapContaining.hasKey;
-//import static org.hamcrest.collection.IsMapContaining.hasValue;
-//import static org.hamcrest.core.Is.is;
-//import static org.junit.Assert.assertThat;
-//
-//public class RepositoryShould {
-//
-//    private Repository repository;
-//
-//    @Before
-//    public void initialise() {
-//        repository = new Repository();
-//    }
-//    @Test public void
-//    return_a_users_posts() {
-//        Post alicePost = new Post("Alice", "Blah", new Clock());
-//        Post fredPost = new Post("Fred", "Blah", new Clock());
-//        Post ottoPost = new Post("Otto", "Blah", new Clock());
-//
-//        repository.add(alicePost);
-//        repository.add(fredPost);
-//        repository.add(ottoPost);
-//
-//        assertThat(repository.allPosts().size(), is(3));
-//    }
-//
+package com.codurance.unittests;
+
+import com.codurance.Clock;
+import com.codurance.Post;
+import com.codurance.Repository;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class RepositoryShould {
+
+    private Repository repository;
+
+    @Before
+    public void initialise() {
+        repository = new Repository();
+    }
+    @Test public void
+    return_a_users_posts() {
+        Post alicePost1 = new Post("Alice", "Blah", new Clock());
+        Post alicePost2 = new Post("Alice", "Blah", new Clock());
+        Post ottoPost = new Post("Otto", "Blah", new Clock());
+
+        repository.add(alicePost1);
+        repository.add(alicePost2);
+        repository.add(ottoPost);
+
+        assertThat(repository.getPostsFor("Alice").size(), is(2));
+        assertThat(repository.getPostsFor("Alice").get(0).getUser(), is("Alice"));
+        assertThat(repository.getPostsFor("Alice").get(1).getUser(), is("Alice"));
+    }
+
 //    @Test public void
 //    create_new_follow_relationship() {
 //        repository.createRelationship("Alice", "Bob");
@@ -47,4 +46,4 @@
 //        assertThat(repository.getRelationships(), hasKey(String.valueOf("Alice")));
 //        assertThat(repository.getRelationships(), hasValue(contains(String.valueOf("Bob"),String.valueOf( "Toby"))));
 //    }
-//}
+}
